43dcee3d326eaae76eca958f06c0149473e2ef1b
Michael Miller-Hairston <michael.millerhairston@gmail.com>
2023-09-04 20:09:54 -0700

    Initial Commit

---
+++ project-chef-gg/project_chef_ggApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_chef_ggApp.swift
+//  project-chef-gg
+//
+//  Created by Michael Miller on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct project_chef_ggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
